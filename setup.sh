a0a2404d90c113b24f7495d085d724c2f7e785d7
Brandon Mailhiot <brandon.mailhiot@gmail.com>
2023-02-08 21:30:12 -0800

    feat: execute all scripts in setup

--- setup.sh
+++ setup.sh
@@ -1,42 +1,4 @@
-#! /bin/bashy
+#! /bin/bash
 
 chmod +x ./*/*.sh
-
-./mac/dock.sh
-./mac/finder.sh
-./mac/homebrew.sh
-
-./terminal/zsh.sh
-./terminal/theme.sh
-./terminal/plugins.sh
-
-./lang_env/python.sh
-./lang_env/nodejs.sh
-./lang_env/golang.sh
-./lang_env/ruby.sh
-./lang_env/java.sh
-./lang_env/elixir.sh
-./lang_env/scala.sh
-./lang_env/clojure.sh
-./lang_env/rust.sh
-./lang_env/haskell.sh
-./lang_env/julia.sh
-
-./infrastructure/terraform.sh
-./infrastructure/ansible.sh
-./infrastructure/packer.sh
-
-./container/docker.sh
-./container/kubernetes.sh
-
-./apps/raycast.sh
-./apps/vscode.sh
-./apps/opera-gx.sh
-./apps/obsidian.sh
-./apps/notion.sh
-./apps/spotify.sh
-./apps/zoom.sh
-./apps/discord.sh
-./apps/slack.sh
-
-./git/git.sh
+find . -name "*.sh" -exec {} \;
